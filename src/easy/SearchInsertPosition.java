e364f022ac191d99f01c253748636cf3e43195af
MarcinG <gawlikmr@gmail.com>
2024-01-25 09:20:22 +0100

    easy - SearchInsertPosition

---
+++ src/easy/SearchInsertPosition.java
@@ -0,0 +1,28 @@
+package easy;
+
+//binary search algorithm with pointers
+class SearchInsertPosition {
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1}, 1));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 7));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 2));
+    }
+
+    private static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
