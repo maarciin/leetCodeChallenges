684e7d96364e9a62df80f822247252b9bc56c449
MarcinG <gawlikmr@gmail.com>
2024-02-20 23:57:15 +0100

    easy - MissingNumber

---
+++ src/easy/MissingNumber.java
@@ -0,0 +1,15 @@
+package easy;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[]{3, 0, 1}));
+        System.out.println(missingNumber(new int[]{0, 1}));
+        System.out.println(missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+    }
+
+    public static int missingNumber(int[] nums) {
+        return nums.length * (nums.length + 1) / 2 - Arrays.stream(nums).sum();
+    }
+}
